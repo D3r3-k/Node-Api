c7455b19b591cab53f805bf5fc6cc609cc4e8521
D3r3k <d3r3k16ib@gmail.com>
2023-01-28 17:31:07 -0600

    borrando comentarios de variables

--- apis/productosAPI.js
+++ apis/productosAPI.js
@@ -60,39 +60,10 @@ const marcas_api = [
 ]
 
 const inventario_api = [
-    // {
-    //     id: 1,
-    //     precio: 300,
-    //     marca: 1,
-    //     linea: 3,
-    //     stock: 10,
-    //     activo: true
-    // }
 ]
 
 const ingresos_api = [
-    // {
-    //     id: 1,
-    //     fecha: '27/01/2023',
-    //     productos: [
-    //         {
-    //             producto_id: 10001,
-    //             cantidad: 50
-    //         }
-    //     ]
-    // }
 ]
 const ventas_api = [
-    // {
-    //     id: 1,
-    //     productos: [
-    //         {
-    //             id: 1,
-    //             cantidad: 3,
-    //             subtotal: 100
-    //         }
-    //     ],
-    //     total: 300
-    // }
 ]
 module.exports = { lineas_api, marcas_api, inventario_api, ingresos_api, ventas_api }
